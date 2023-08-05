18bd69a05eb744f259f8814e228e426f3ce3d0b2
Artur Dornelles de Moura <artur.dornelles@gmail.com>
2023-08-05 16:05:31 -0300

    Initial Commit

---
+++ ios-movie-list/ios_movie_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_movie_listApp.swift
+//  ios-movie-list
+//
+//  Created by Artur Moura on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_movie_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
